b0e9edbb1bc83a44a023ce437d2c6f0e8810075c
Jinho Jang <verycosy@kakao.com>
2021-05-10 21:15:42 +0900

    refactor: 날짜 구하는 함수 분리

--- mqttClient.js
+++ mqttClient.js
@@ -2,6 +2,7 @@
 const mqtt = require("mqtt"); //(2)mqtt 모듈을 먼저 불러오는 작업이다.
 const client = mqtt.connect(process.env.MQTT_URL); //(3)가장 먼저 수행을 해야할 것은 바로 mqtt와 접속할 수 있는 client를 만들어 주는 작업을 수행해야한다. 이 말이 바로 nodejs와 mqtt 쪽에서 connect를 하겠다라는 의미라고 생각하시면 쉽습니다.
 const DHT11 = require("./models/DHT11"); //(18)모듈을 불러오고 이 객체의 이름을 DHT11로 하겠다.
+const { getCurrentUTCDate } = require("./lib/utils");
 
 //(4) nodejs ---> mosquitto server 즉 Nodejs에서 mosquitto server로 접속을 하는 작업이다.
 client.on("connect", () => {
@@ -13,19 +14,12 @@ client.on("connect", () => {
 client.on("message", (topic, message) => {
   //(8) {"tmp":25.00,"hum":37.00} =>JSON(Object) //message가 들어오면 아래의 작업을 수행을 하는데 우리가 mqtt에서 배웠을때 예시로 dht11 이라고 선언한 토픽에 대한 내용도 추가를 해줘야만 한다. 그래서 토픽과, 실제 값이 들어가는 인자 값 까지 챙겨야하니 이 부분을 참고한다.
   const { tmp, hum } = JSON.parse(message); //(9) 이작업은 mqtt에서 들어온 msg가 {"tmp":25.00,"hum":37.00}라는 문자열로 들어온다. 그런데 이건 너무 귀찮으니 JSON 형태로 바꿔주면 속성값으로 취급이 가능하기가 용이하기에 JSON으로 바꿔주는 작업을 수행한다.
-  const date = new Date(); //(10)우리는 tmp랑 hum 값만 취급했는데 여기에 date를 집어 넣고
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const today = date.getDate();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds(); //(11)표준시차
 
   const dht11 = new DHT11({
     //(19)객체를 생성을 하고
     tmp, //20_값들을 집어 넣는 작업이다.
     hum,
-    created_at: new Date(Date.UTC(year, month, today, hours, minutes, seconds)),
+    created_at: getCurrentUTCDate(),
   });
   try {
     const saveDHT11 = dht11.save(); //21_DB에 저장한 다음에
